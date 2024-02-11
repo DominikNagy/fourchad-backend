1700cb6b0ba0535375a811205a54586509aea040
Dominik Nagy <nagy@essential-data.sk>
2024-02-11 22:16:40 +0100

    generated clean project and updated .gitignore

---
+++ src/main/java/com/dominiknagy/fourchad/FourchadApplication.java
@@ -0,0 +1,13 @@
+package com.dominiknagy.fourchad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourchadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourchadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominiknagy/fourchad/FourchadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominiknagy.fourchad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourchadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
