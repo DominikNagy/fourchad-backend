e5ee8fccac3aa64d5b92608308135dbeb1bdb28e
Dominik Nagy <nagy@essential-data.sk>
2024-02-12 19:39:20 +0100

    request mappings

--- src/main/java/com/dominiknagy/fourchad/controllers/AdminController.java
+++ src/main/java/com/dominiknagy/fourchad/controllers/AdminController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin")
 @CrossOrigin(origins = "*")
+@RequestMapping("/api/admin")
 public class AdminController {
 
     private final BoardService boardService;
